85719da23d0033b3347ec478477f33769d15d8d3
bdrtr <bedirkaraabali@gmail.com>
2025-08-07 00:05:47 +0300

    solved tile

--- src/tile.go
+++ src/tile.go
@@ -1,10 +1,12 @@
 package src
 
+type Tile struct {
 	BlockID     uint16
 	BlockActive bool
 	// Diğer tile özellikleri eklenebilir (ör. wall, liquid, vb.)
 }
 
+type TileMatrix struct {
 	Tiles [][]Tile
 }
 
@@ -12,6 +14,6 @@ func NewTileMatrix(width, height int) *TileMatrix {
 	tiles := make([][]Tile, width)
 	for i := range tiles {
 		tiles[i] = make([]Tile, height)
-	}
+	} 
 	return &TileMatrix{Tiles: tiles}
 }
